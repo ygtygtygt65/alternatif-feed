f4d4ef94877631f30cbbc746e9da115b361067c0
ygtygtygt65 <161406290+ygtygtygt65@users.noreply.github.com>
2025-07-19 02:42:44 +0300

    Update diken.js

--- sources/diken.js
+++ sources/diken.js
@@ -1,1 +1,23 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+export default async function scrapeDiken() {
+  const url = 'https://www.diken.com.tr';
+  const { data: html } = await axios.get(url);
+  const $ = cheerio.load(html);
+
+  const items = [];
+
+  $('.td-big-grid-post .td-module-thumb a').each((i, el) => {
+    const title = $(el).attr('title')?.trim();
+    const link = $(el).attr('href');
+    const pubDate = new Date().toUTCString(); // Tahmini tarih
+
+    if (title && link) {
+      items.push({ title, link, pubDate });
+    }
+  });
+
+  return items;
+}
 
